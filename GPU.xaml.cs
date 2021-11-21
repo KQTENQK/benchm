fc9f2a8fbcb1d503283e80ec6f85631b03b6e5dc
KQTENQK <xsx3xx@gmail.com>
2021-11-21 18:44:48 +0300

    init

---
+++ GPU.xaml.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Media3D;
+using System.Windows.Threading;
+
+namespace LLAb2
+{
+    public partial class GPU : Window
+    {
+        private TimeSpan _testDuration = new TimeSpan(0, 0, 30);
+        private int _frames = 0;
+
+        public event Action<int> Ended;
+
+        public GPU()
+        {
+            InitializeComponent();
+
+            DirectionalLight dl = new DirectionalLight(Colors.White, new Vector3D(-1, -1, -1));
+            dl.Color = Colors.Pink;
+
+            _light.Content = dl;
+
+            Model3DGroup model = new Model3DGroup();
+
+            for (float x = -2; x < 2; x += 0.35f)
+            {
+                for (float y = -2; y < 2; y += 0.35f)
+                {
+                    for (float z = -2; z < 2; z += 0.35f)
+                    {
+                        model.Children.Add(InstantiateGeometry(x, y, z));
+                    }
+                }
+            }
+
+            CompositionTarget.Rendering += (s, a) =>
+            {
+                ++_frames;
+            };
+
+            DispatcherTimer fpsTimer = new DispatcherTimer();
+            fpsTimer.Interval = _testDuration;
+            fpsTimer.Tick += (s, a) =>
+            {
+                Ended?.Invoke(_frames);
+                Close();
+            };
+            fpsTimer.Start();
+
+            _meshObjects.Content = model;
+
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.To = 40 * _testDuration.Seconds;
+            animation.DecelerationRatio = 1;
+            animation.Duration = TimeSpan.FromSeconds(_testDuration.Seconds);
+            animation.AutoReverse = false;
+            AxisAngleRotation3D rotateTransform = new AxisAngleRotation3D(new Vector3D(0, 1, 1), 5);
+            model.Transform = new RotateTransform3D(rotateTransform);
+            rotateTransform.BeginAnimation(AxisAngleRotation3D.AngleProperty, animation);
+        }
+
+        public Model3DGroup InstantiateGeometry(float x, float y, float z)
+        {
+            GeometryModel3D firstModel = new GeometryModel3D();
+            GeometryModel3D secondModel = new GeometryModel3D();
+            GeometryModel3D thirdModel = new GeometryModel3D();
+            GeometryModel3D fourthModel = new GeometryModel3D();
+            GeometryModel3D fifthModel = new GeometryModel3D();
+            GeometryModel3D sixthModel = new GeometryModel3D();
+            GeometryModel3D seventhModel = new GeometryModel3D();
+            GeometryModel3D eighthModel = new GeometryModel3D();
+
+            MeshGeometry3D first = new MeshGeometry3D();
+            Point3DCollection pointsFirst = new Point3DCollection();
+
+            pointsFirst.Add(new Point3D(0.0 + x, 0.0 + y, 1.0 + z));
+            pointsFirst.Add(new Point3D(0.0 + x, 1.0 + y, 0.0 + z));
+            pointsFirst.Add(new Point3D(1.0 + x, 0.0 + y, 0.0 + z));
+
+            first.Positions = pointsFirst;
+
+            MeshGeometry3D second = new MeshGeometry3D();
+            Point3DCollection pointsSecond = new Point3DCollection();
+
+            pointsSecond.Add(new Point3D(1.0 + x, 0.0 + y, 0.0 + z));
+            pointsSecond.Add(new Point3D(0.0 + x, 1.0 + y, 0.0 + z));
+            pointsSecond.Add(new Point3D(0.0 + x, 0.0 + y, -1.0 + z));
+
+            second.Positions = pointsSecond;
+
+            MeshGeometry3D third = new MeshGeometry3D();
+            Point3DCollection pointsThird = new Point3DCollection();
+
+            pointsThird.Add(new Point3D(-1.0 + x, 0.0 + y, 0.0 + z));
+            pointsThird.Add(new Point3D(0.0 + x, 1.0 + y, 0.0 + z));
+            pointsThird.Add(new Point3D(0.0 + x, 0.0 + y, -1.0 + z));
+
+            third.Positions = pointsThird;
+
+            MeshGeometry3D fourth = new MeshGeometry3D();
+            Point3DCollection pointsFourth = new Point3DCollection();
+
+            pointsFourth.Add(new Point3D(0.0 + x, 0.0 + y, 1.0 + z));
+            pointsFourth.Add(new Point3D(0.0 + x, 1.0 + y, 0.0 + z));
+            pointsFourth.Add(new Point3D(-1.0 + x, 0.0 + y, 0.0 + z));
+
+            fourth.Positions = pointsFourth;
+
+            MeshGeometry3D fifth = new MeshGeometry3D();
+            Point3DCollection pointsFifth = new Point3DCollection();
+
+            pointsFifth.Add(new Point3D(0.0 + x, 0.0 + y, 1.0 + z));
+            pointsFifth.Add(new Point3D(0.0 + x, -1.0 + y, 0.0 + z));
+            pointsFifth.Add(new Point3D(1.0 + x, 0.0 + y, 0.0 + z));
+
+            fifth.Positions = pointsFifth;
+
+            MeshGeometry3D sixth = new MeshGeometry3D();
+            Point3DCollection pointsSixth = new Point3DCollection();
+
+            pointsSixth.Add(new Point3D(1.0 + x, 0.0 + y, 0.0 + z));
+            pointsSixth.Add(new Point3D(0.0 + x, -1.0 + y, 0.0 + z));
+            pointsSixth.Add(new Point3D(0.0 + x, 0.0 + y, -1.0 + z));
+
+            sixth.Positions = pointsSixth;
+
+            MeshGeometry3D seventh = new MeshGeometry3D();
+            Point3DCollection pointsSeventh = new Point3DCollection();
+
+            pointsSeventh.Add(new Point3D(-1.0 + x, 0.0 + y, 0.0 + z));
+            pointsSeventh.Add(new Point3D(0.0 + x, -1.0 + y, 0.0 + z));
+            pointsSeventh.Add(new Point3D(0.0 + x, 0.0 + y, -1.0 + z));
+
+            seventh.Positions = pointsSeventh;
+
+            MeshGeometry3D eighth = new MeshGeometry3D();
+            Point3DCollection pointsEighth = new Point3DCollection();
+
+            pointsEighth.Add(new Point3D(0.0 + x, 0.0 + y, 1.0 + z));
+            pointsEighth.Add(new Point3D(0.0 + x, -1.0 + y, 0.0 + z));
+            pointsEighth.Add(new Point3D(-1.0 + x, 0.0 + y, 0.0 + z));
+
+            eighth.Positions = pointsEighth;
+
+            Int32Collection indencies = new Int32Collection(3);
+
+            indencies.Add(0);
+            indencies.Add(2);
+            indencies.Add(1);
+
+            first.TriangleIndices = indencies;
+            second.TriangleIndices = indencies;
+            third.TriangleIndices = indencies;
+            fourth.TriangleIndices = indencies;
+            fifth.TriangleIndices = indencies;
+            sixth.TriangleIndices = indencies;
+            seventh.TriangleIndices = indencies;
+            eighth.TriangleIndices = indencies;
+
+            firstModel.Geometry = first;
+            secondModel.Geometry = second;
+            thirdModel.Geometry = third;
+            fourthModel.Geometry = fourth;
+            fifthModel.Geometry = fifth;
+            sixthModel.Geometry = sixth;
+            seventhModel.Geometry = seventh;
+            eighthModel.Geometry = eighth;
+
+            firstModel.Material = new DiffuseMaterial(Brushes.Cyan);
+            secondModel.Material = new DiffuseMaterial(Brushes.Cyan);
+            thirdModel.Material = new DiffuseMaterial(Brushes.Cyan);
+            fourthModel.Material = new DiffuseMaterial(Brushes.Cyan);
+            fifthModel.Material = new DiffuseMaterial(Brushes.Cyan);
+            sixthModel.Material = new DiffuseMaterial(Brushes.Cyan);
+            seventhModel.Material = new DiffuseMaterial(Brushes.Cyan);
+            eighthModel.Material = new DiffuseMaterial(Brushes.Cyan);
+
+            Model3DGroup models = new Model3DGroup();
+
+            models.Children.Add(firstModel);
+            models.Children.Add(secondModel);
+            models.Children.Add(thirdModel);
+            models.Children.Add(fourthModel);
+            models.Children.Add(fifthModel);
+            models.Children.Add(sixthModel);
+            models.Children.Add(seventhModel);
+            models.Children.Add(eighthModel);
+
+            return models;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace LLAb2
+{
+    public partial class MainWindow : Window
+    {
+        private List<int[]> _arrays = new List<int[]>();
+        private DataTable _table = new DataTable();
+        private DataTable _normalizedTable = new DataTable();
+        private Window _processingMark;
+        private int _cpu = 0;
+        private int _memory = 0;
+        private int _frames = 0;
+        private string _name = "My";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeDataGrid();
+            Hide();
+            _processingMark = new ProcessingMarkWindow();
+            _processingMark.Show();
+            CPU();
+            Memory();
+            var gpu = new GPU();
+            gpu.Ended += OnGpuTestEnded;
+            gpu.ShowDialog();
+
+            _processingMark.Close();
+            
+
+            _table.Rows.Add(_name, _cpu, _frames, _memory, (_cpu + _frames + _memory) / 3);
+
+            int maxCPU = 0;
+            int maxGPU = 0;
+            int maxMemory = 0;
+            int maxOverall = 0;
+
+            for (int i = 0; i < _table.Rows.Count - 1; i++)
+            {
+                if (int.Parse(_table.Rows[i][1].ToString()) > maxCPU)
+                    maxCPU = int.Parse(_table.Rows[i][1].ToString());
+
+                if (int.Parse(_table.Rows[i][2].ToString()) > maxGPU)
+                    maxGPU = int.Parse(_table.Rows[i][2].ToString());
+
+                if (int.Parse(_table.Rows[i][3].ToString()) > maxMemory)
+                    maxMemory = int.Parse(_table.Rows[i][3].ToString());
+
+                if (int.Parse(_table.Rows[i][4].ToString()) > maxOverall)
+                    maxOverall = int.Parse(_table.Rows[i][4].ToString());
+            }
+
+            for (int i = 0; i < _table.Rows.Count - 1; i++)
+            {
+                float normalizedCPU = (float)int.Parse(_table.Rows[i][1].ToString()) / (float)maxCPU;
+                float normalizedGPU = (float)int.Parse(_table.Rows[i][2].ToString()) / (float)maxGPU;
+                float normalizedMemory = (float)int.Parse(_table.Rows[i][3].ToString()) / (float)maxMemory;
+                float normalizedOverall = (float)int.Parse(_table.Rows[i][4].ToString()) / (float)maxOverall;
+
+                _normalizedTable.Rows.Add($"PC{i + 1}", normalizedCPU, normalizedGPU, normalizedMemory, normalizedOverall);
+            }
+
+            float normalizedCPUThis = (float)int.Parse(_table.Rows[_table.Rows.Count - 1][1].ToString()) / (float)maxCPU;
+            float normalizedGPUThis = (float)int.Parse(_table.Rows[_table.Rows.Count - 1][2].ToString()) / (float)maxGPU;
+            float normalizedMemoryThis = (float)int.Parse(_table.Rows[_table.Rows.Count - 1][3].ToString()) / (float)maxMemory;
+            float normalizedOverallThis = (float)int.Parse(_table.Rows[_table.Rows.Count - 1][4].ToString()) / (float)maxOverall;
+
+            _normalizedTable.Rows.Add(_name, normalizedCPUThis, normalizedGPUThis, normalizedMemoryThis, normalizedOverallThis);
+
+            Show();
+        }
+
+        private void InitializeDataGrid()
+        {
+
+            _table.Columns.Add("Name");
+            _table.Columns.Add("CPU Time");
+            _table.Columns.Add("GPU Time");
+            _table.Columns.Add("Memory Time");
+            _table.Columns.Add("Overall");
+            _resultData.ItemsSource = _table.DefaultView;
+
+            _table.Rows.Add("PC1", 50583, 54341, 33096, 46006);
+            _table.Rows.Add("PC2", 74339, 111495, 68412, 84748);
+            _table.Rows.Add("PC3", 28855, 40142, 23295, 30764);
+
+            _normalizedTable.Columns.Add("Name");
+            _normalizedTable.Columns.Add("CPU Time");
+            _normalizedTable.Columns.Add("GPU Time");
+            _normalizedTable.Columns.Add("Memory Time");
+            _normalizedTable.Columns.Add("Overall");
+
+            _normalizedResultData.ItemsSource = _normalizedTable.DefaultView;
+        }
+
+        public void CPU()
+        {
+            Random random = new Random();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int n = 0; n < 10000; n++)
+            {
+                for (int i = 1; i < 300; i++)
+                {
+                    Factorial(i);
+                }
+            }
+
+            for (int i = 0; i < 10000; i++)
+            {
+                int[] array = new int[16384];
+
+                for (int j = 0; j < 16384; j++)
+                {
+                    array[j] = random.Next(-16384, 16384);
+                }
+
+                Brush(array);
+                _arrays.Add(array);
+            }
+
+            stopwatch.Stop();
+            _cpu = stopwatch.Elapsed.Seconds * 1000 + stopwatch.Elapsed.Milliseconds;
+        }
+
+        public void Memory()
+        {
+            string filePath = @"C:\BenchMark\Test.txt";
+            string directory = @"C:\BenchMark";
+            Directory.CreateDirectory(directory);
+
+            TimeSpan testDuration = new TimeSpan(0, 0, 5);
+            Stopwatch stopwatch = new Stopwatch();
+
+            try
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(filePath))
+                    {
+                        stopwatch.Start();
+
+                        foreach (var array in _arrays)
+                        {
+                            for (int j = 0; j < array.Length; j++)
+                            {
+                                streamWriter.WriteLine(array[j].ToString());
+                            }
+                        }
+                    }
+
+                    using (StreamReader streamReader = new StreamReader(filePath))
+                    {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.Append(streamReader.ReadToEnd());
+                    }
+                }
+            }
+            catch { }
+
+            stopwatch.Stop();
+            _memory = stopwatch.Elapsed.Seconds * 1000 + stopwatch.Elapsed.Milliseconds;
+
+            File.Delete(filePath);
+            Directory.Delete(directory);
+        }
+
+        public void Brush(int[] array)
+        {
+            double factor = 1.247330950103979;
+            int step = array.Length - 1;
+
+            while (step >= 1)
+            {
+                for (int i = 0; i + step < array.Length; i++)
+                {
+                    if (array[i] > array[i + step])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i + step];
+                        array[i + step] = temp;
+                    }
+                }
+
+                step = (int)((double)step / factor);
+            }
+        }
+
+        public int Factorial(int n)
+        {
+            if (n == 1)
+                return 1;
+
+            return n * Factorial(n - 1);
+        }
+
+        private void OnGpuTestEnded(int frames)
+        {
+            _frames = frames;
+            _frames *= 300;
+        }
+    }
+}
